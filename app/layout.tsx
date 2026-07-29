4fab8bc7e1a615319ac5e6d2b0d4bae8c5c43a4d
Sara Maryam <227032307+saramaryam2005@users.noreply.github.com>
2026-07-29 22:44:55 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,3 +21,13 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
